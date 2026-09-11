4e0aad61ed0102284ba4eccbb999f5ca2e007301
Stella Wong Kai Ning <swon0163@student.monash.edu>
2026-09-11 09:44:08 +0800

    Set up Flutter dart

---
+++ lumi/lib/config/environment.dart
@@ -0,0 +1,3 @@
+APPWRITE_PROJECT_ID="6aa3507d00149bb41795"
+APPWRITE_PROJECT_NAME="lumi"
+APPWRITE_ENDPOINT="https://sgp.cloud.appwrite.io/v1"
---
+++ lumi/lib/services/appwrite_client.dart
@@ -0,0 +1,2 @@
+Client().setEndpoint('https://cloud.appwrite.io/v1').setProject('6aa3507d00149bb41795'); 
+Account account = Account(client);
