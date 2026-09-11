682da0510ffd8965baec8b5344f03d99ba736323
Stella Wong Kai Ning <swon0163@student.monash.edu>
2026-09-11 10:13:02 +0800

    Connect to Appwrite

--- lumi/lib/config/environment.dart
+++ lumi/lib/config/environment.dart
@@ -1,3 +1,3 @@
-APPWRITE_PROJECT_ID="6aa3507d00149bb41795"
-APPWRITE_PROJECT_NAME="lumi"
-APPWRITE_ENDPOINT="https://sgp.cloud.appwrite.io/v1"
+const String APPWRITE_PROJECT_ID = "6aa3507d00149bb41795";
+const String APPWRITE_PROJECT_NAME = "lumi";
+const String APPWRITE_ENDPOINT = "https://sgp.cloud.appwrite.io/v1";
