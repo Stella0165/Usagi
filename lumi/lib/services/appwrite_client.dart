bdd4c84086a9a68a76a91381edf31dc201be3be2
Stella Wong Kai Ning <swon0163@student.monash.edu>
2026-09-13 13:10:26 +0800

    Change env names

--- lumi/lib/services/appwrite_client.dart
+++ lumi/lib/services/appwrite_client.dart
@@ -4,8 +4,8 @@ import '../config/environment.dart';
 
 class Appwrite {
   static final Client client = Client()
-      .setEndpoint(APPWRITE_ENDPOINT)
-      .setProject(APPWRITE_PROJECT_ID);
+      .setEndpoint(LUMI_APPWRITE_ENDPOINT)
+      .setProject(LUMI_APPWRITE_PROJECT_ID);
 
   static final Account account = Account(client);
   static final Databases databases = Databases(client);
